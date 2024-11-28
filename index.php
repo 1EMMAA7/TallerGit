5c24e22c72a0a7a0c763b75ad2c035bee315ee24
1EMMAA7 <emmaanuelbarragan@gmail.com>
2024-11-28 10:13:33 -0600

    Agrege mi nombre al proyecto

--- index.php
+++ index.php
@@ -6,14 +6,17 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="..CSS/ST">
+    <link rel="stylesheet" href="style.css">
     <title>PROYECTO</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   </head>
   <body>
+
   <div class="container_flui" style="background:purple;text-align: center;">
 
-    <h2>BIENVENIDO A ITSAO</h2>
-    <img src="itsao.png" alt="">
+    <h1>BIENVENIDO A ITSAO</h1>
+    <h1>EMMANUEL BARRAGÁN</h1>
+    <img src="Cliente/itsao.png" alt="" style="filter: drop-shadow(0 0 10px rgba(0,0,0,1))">  
     </div>
 
     <div class="container" style="padding: 50px;">
@@ -63,6 +66,7 @@
 
 
   </div>
+  
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
   </body>
 </html>
