e6b0c5401b98c10bd3de4fbac4939127fbd8770f
1EMMAA7 <emmaanuelbarragan@gmail.com>
2024-11-27 18:16:46 -0600

    agregando proyecto completo

---
+++ index.php
@@ -0,0 +1,35 @@
+
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="..CSS/ST">
+    <title>PROYECTO</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  </head>
+  <body>
+  <div class="container_flui" style="background:purple;text-align: center;">
+
+    <h2>BIENVENIDO A ITSAO</h2>
+    <img src="itsao.png" alt="">
+    </div>
+
+    <div class="container" style="padding: 50px;">
+    <div class="row">
+  <div class="col">
+    <input type="text" class="form-control" placeholder="First name" aria-label="First name">
+  </div>
+  <div class="col">
+    <input type="text" class="form-control" placeholder="Last name" aria-label="Last name">
+  </div>
+</div>
+    </div>
+
+
+
+  </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+  </body>
+</html>
